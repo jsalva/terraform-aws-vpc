38e4802f54529d15a80aa0e17e9b4a6cdd6adb63
John Salvatore <john@pymetrics.com>
2016-12-16 17:15:52 -0500

    dynamically generates resources with AZs as input

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Terraform module: AWS VPC
 
-Creates a VPC with private and public subnets in each availability zone for the specified region.  Also starts a NAT instance to allow outside communication from private subnets.
+Creates a VPC with private and public subnets in each availability zone for the specified region.  Also starts a NAT Gateway to allow outside communication from private subnets.
 
-Required variables are `vpc_name`, `vpc_region` and `vpc_nat_key_file`.
+Required variables are `vpc_name`, `vpc_region`.
--- main.tf
+++ main.tf
@@ -11,6 +11,11 @@ variable "vpc_region" {
     description = "Target region for the VPC"
 }
 
+variable "availability_zones" {
+    description = "Will launch parallel resources in these Availability Zones"
+    type = "list"
+}
+
 #########
 ## VPC ##
 #########
@@ -40,53 +45,15 @@ resource "aws_vpc" "main" {
 ## Private Subnets ##
 #####################
 
-resource "aws_subnet" "private_1" {
-
+resource "aws_subnet" "private" {
+    count = "${length(var.availability_zones)}"
     vpc_id = "${aws_vpc.main.id}"
-    availability_zone = "${var.vpc_region}a"
-    cidr_block = "10.0.1.0/24"
+    availability_zone = "${element(var.availability_zones, count.index)}"
+    cidr_block = "10.0.${count.index+1}.0/24"
     map_public_ip_on_launch = false
 
     tags {
-        Name = "${var.vpc_name}-Private Subnet 1"
-        VPC = "${var.vpc_name}"
-        ManagedBy = "terraform"
-    }
-
-    lifecycle {
-        create_before_destroy = true
-    }
-
-}
-
-resource "aws_subnet" "private_2" {
-
-    vpc_id = "${aws_vpc.main.id}"
-    availability_zone = "${var.vpc_region}b"
-    cidr_block = "10.0.2.0/24"
-    map_public_ip_on_launch = false
-
-    tags {
-        Name = "${var.vpc_name}-Private Subnet 2"
-        VPC = "${var.vpc_name}"
-        ManagedBy = "terraform"
-    }
-
-    lifecycle {
-        create_before_destroy = true
-    }
-
-}
-
-resource "aws_subnet" "private_3" {
-
-    vpc_id = "${aws_vpc.main.id}"
-    availability_zone = "${var.vpc_region}c"
-    cidr_block = "10.0.3.0/24"
-    map_public_ip_on_launch = false
-
-    tags {
-        Name = "${var.vpc_name}-Private Subnet 3"
+        Name = "${var.vpc_name}-Private Subnet ${count.index+1}"
         VPC = "${var.vpc_name}"
         ManagedBy = "terraform"
     }
@@ -101,53 +68,16 @@ resource "aws_subnet" "private_3" {
 ## Public Subnets ##
 ####################
 
-resource "aws_subnet" "public_1" {
+resource "aws_subnet" "public" {
 
+    count = "${length(var.availability_zones)}"
     vpc_id = "${aws_vpc.main.id}"
-    availability_zone = "${var.vpc_region}a"
-    cidr_block = "10.0.11.0/24"
+    availability_zone = "${element(var.availability_zones, count.index)}"
+    cidr_block = "10.0.${count.index+1}${count.index+1}.0/24"
     map_public_ip_on_launch = true
 
     tags {
-        Name = "${var.vpc_name}-Public Subnet 1"
-        VPC = "${var.vpc_name}"
-        ManagedBy = "terraform"
-    }
-
-    lifecycle {
-        create_before_destroy = true
-    }
-
-}
-
-resource "aws_subnet" "public_2" {
-
-    vpc_id = "${aws_vpc.main.id}"
-    availability_zone = "${var.vpc_region}b"
-    cidr_block = "10.0.22.0/24"
-    map_public_ip_on_launch = true
-
-    tags {
-        Name = "${var.vpc_name}-Public Subnet 2"
-        VPC = "${var.vpc_name}"
-        ManagedBy = "terraform"
-    }
-
-    lifecycle {
-        create_before_destroy = true
-    }
-
-}
-
-resource "aws_subnet" "public_3" {
-
-    vpc_id = "${aws_vpc.main.id}"
-    availability_zone = "${var.vpc_region}c"
-    cidr_block = "10.0.33.0/24"
-    map_public_ip_on_launch = true
-
-    tags {
-        Name = "${var.vpc_name}-Public Subnet 3"
+        Name = "${var.vpc_name}-Public Subnet ${count.index+1}"
         VPC = "${var.vpc_name}"
         ManagedBy = "terraform"
     }
@@ -182,17 +112,21 @@ resource "aws_internet_gateway" "gateway" {
 ## NAT Gateway EIPs ##
 ######################
 
-resource "aws_eip" "nat_gateway_1" {
+resource "aws_eip" "nat_gateway" {
+
     vpc = true
+
 }
 
 ##################
 ## NAT Gateways ##
 ##################
 
-resource "aws_nat_gateway" "nat_gateway_1" {
-    subnet_id = "${aws_subnet.public_1.id}"
-    allocation_id = "${aws_eip.nat_gateway_1.id}"
+resource "aws_nat_gateway" "nat_gateway" {
+
+    subnet_id = "${aws_subnet.public.1.id}"
+    allocation_id = "${aws_eip.nat_gateway.id}"
+
 }
 
 ##################
@@ -211,13 +145,13 @@ resource "aws_main_route_table_association" "main_routes" {
 ## Route tables: private instances ##
 #####################################
 
-# Routes traffic through the NAT gateway
+# Routes traffic through the NAT Gateway
 resource "aws_route_table" "private_routes" {
 
     vpc_id = "${aws_vpc.main.id}"
     route {
         cidr_block = "0.0.0.0/0"
-        nat_gateway_id = "${aws_nat_gateway.nat_gateway_1.id}"
+        nat_gateway_id = "${aws_nat_gateway.nat_gateway.id}"
     }
 
     tags {
@@ -228,29 +162,11 @@ resource "aws_route_table" "private_routes" {
 
 }
 
-# Subnet associations
-# Private subnet 1
-resource "aws_route_table_association" "private_a1" {
+# Private subnet associations
+resource "aws_route_table_association" "private" {
 
-    subnet_id = "${aws_subnet.private_1.id}"
-    route_table_id = "${aws_route_table.private_routes.id}"
-
-}
-
-# Subnet associations
-# Private subnet 2
-resource "aws_route_table_association" "private_a2" {
-
-    subnet_id = "${aws_subnet.private_2.id}"
-    route_table_id = "${aws_route_table.private_routes.id}"
-
-}
-
-# Subnet associations
-# Private subnet 3
-resource "aws_route_table_association" "private_a3" {
-
-    subnet_id = "${aws_subnet.private_3.id}"
+    count = "${length(var.availability_zones)}"
+    subnet_id = "${element(aws_subnet.private.*.id,count.index)}"
     route_table_id = "${aws_route_table.private_routes.id}"
 
 }
@@ -276,29 +192,11 @@ resource "aws_route_table" "public_routes" {
 
 }
 
-# Subnet associations
-# Public subnet 1
-resource "aws_route_table_association" "public_a1" {
+# Public subnet associations
+resource "aws_route_table_association" "public" {
 
-    subnet_id = "${aws_subnet.public_1.id}"
-    route_table_id = "${aws_route_table.public_routes.id}"
-
-}
-
-# Subnet associations
-# Public subnet 2
-resource "aws_route_table_association" "public_a2" {
-
-    subnet_id = "${aws_subnet.public_2.id}"
-    route_table_id = "${aws_route_table.public_routes.id}"
-
-}
-
-# Subnet associations
-# Public subnet 3
-resource "aws_route_table_association" "public_a3" {
-
-    subnet_id = "${aws_subnet.public_3.id}"
+    count = "${length(var.availability_zones)}"
+    subnet_id = "${element(aws_subnet.public.*.id,count.index)}"
     route_table_id = "${aws_route_table.public_routes.id}"
 
 }
@@ -316,17 +214,17 @@ output "vpc_region" {
 }
 
 output "vpc_private_subnets" {
-    value = "${aws_subnet.private_1.cidr_block},${aws_subnet.private_2.cidr_block},${aws_subnet.private_3.cidr_block}"
+    value = "${list(aws_subnet.private.*.cidr_block)}"
 }
 
 output "vpc_private_subnet_ids" {
-    value = "${aws_subnet.private_1.id},${aws_subnet.private_2.id},${aws_subnet.private_3.id}"
+    value = "${list(aws_subnet.private.*.id)}"
 }
 
 output "vpc_public_subnets" {
-    value = "${aws_subnet.public_1.cidr_block},${aws_subnet.public_2.cidr_block},${aws_subnet.public_3.cidr_block}"
+    value = "${list(aws_subnet.public.*.cidr_block)}"
 }
 
 output "vpc_public_subnet_ids" {
-    value = "${aws_subnet.public_1.id},${aws_subnet.public_2.id},${aws_subnet.public_3.id}"
+    value = "${list(aws_subnet.public.*.id)}"
 }
